21497cfde1e4f808af6ee961d4e808ec05556a1e
Sun Kit Tsui <sunkittsui@gmail.com>
2023-12-02 15:30:27 -0600

    Solve 2023 day2 part2

--- src/year_2023/day1.rs
+++ src/year_2023/day1.rs
@@ -3,8 +3,7 @@ pub fn solve_part1(input: &str) -> i32 {
 }
 
 pub fn solve_part2(input: &str) -> i32 {
-    let parsed = parse_input(input);
-    todo!()
+    parse_input2(input)
 }
 
 fn parse_input(input: &str) -> i32 {
@@ -23,9 +22,98 @@ fn parse_input(input: &str) -> i32 {
         .sum()
 }
 
+fn parse_input2(input: &str) -> i32 {
+    input.split("\n").map(parse_line).flatten().sum()
+}
+
+fn parse_line(line: &str) -> Option<i32> {
+    let mut num_str = String::new();
+    // find front
+    for i in 0..line.len() {
+        let sub_str = &line[i..];
+        if let Some(c) = sub_str.chars().next() {
+            if c.is_numeric() {
+                num_str.push(c);
+                break;
+            }
+        }
+
+        if sub_str.starts_with("one") {
+            num_str.push('1');
+            break;
+        } else if sub_str.starts_with("two") {
+            num_str.push('2');
+            break;
+        } else if sub_str.starts_with("three") {
+            num_str.push('3');
+            break;
+        } else if sub_str.starts_with("four") {
+            num_str.push('4');
+            break;
+        } else if sub_str.starts_with("five") {
+            num_str.push('5');
+            break;
+        } else if sub_str.starts_with("six") {
+            num_str.push('6');
+            break;
+        } else if sub_str.starts_with("seven") {
+            num_str.push('7');
+            break;
+        } else if sub_str.starts_with("eight") {
+            num_str.push('8');
+            break;
+        } else if sub_str.starts_with("nine") {
+            num_str.push('9');
+            break;
+        }
+    }
+
+    // find back
+    for i in (0..line.len()).rev() {
+        let sub_str = &line[..=i];
+        if let Some(c) = sub_str.chars().next_back() {
+            if c.is_numeric() {
+                num_str.push(c);
+                break;
+            }
+        }
+
+        if sub_str.ends_with("one") {
+            num_str.push('1');
+            break;
+        } else if sub_str.ends_with("two") {
+            num_str.push('2');
+            break;
+        } else if sub_str.ends_with("three") {
+            num_str.push('3');
+            break;
+        } else if sub_str.ends_with("four") {
+            num_str.push('4');
+            break;
+        } else if sub_str.ends_with("five") {
+            num_str.push('5');
+            break;
+        } else if sub_str.ends_with("six") {
+            num_str.push('6');
+            break;
+        } else if sub_str.ends_with("seven") {
+            num_str.push('7');
+            break;
+        } else if sub_str.ends_with("eight") {
+            num_str.push('8');
+            break;
+        } else if sub_str.ends_with("nine") {
+            num_str.push('9');
+            break;
+        }
+    }
+
+    return Some(num_str.parse::<i32>().ok()?);
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::year_2023::day1::parse_input;
+    use crate::year_2023::day1::*;
 
     #[test]
     fn can_solve_part1() {
@@ -35,5 +123,18 @@ a1b2c3d4e5f
 treb7uchet"#;
 
         assert_eq!(parse_input(sample_input), 142);
+    }
+
+    #[test]
+    fn can_solve_part2() {
+        let sample_input = r#"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"#;
+
+        assert_eq!(parse_input2(sample_input), 281);
     }
 }
---
+++ src/year_2023/day2.rs
@@ -0,0 +1,124 @@
+pub fn solve_part1(input: &str) -> i32 {
+    parse_input(input)
+}
+
+pub fn solve_part2(input: &str) -> i32 {
+    parse_input2(input)
+}
+
+#[derive(Debug)]
+struct MaxGameRecord {
+    id: i32,
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl TryFrom<&str> for MaxGameRecord {
+    type Error = String;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let (id_str, values_str) = value
+            .split_once(": ")
+            .ok_or_else(|| "Failed to split initially at ': '")?;
+
+        let (_, id) = id_str
+            .split_once(" ")
+            .ok_or_else(|| "Failed to split `id_str`")?;
+
+        let id = id
+            .parse::<i32>()
+            .expect(&format!("Failed to parse: {}", id));
+
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        for set in values_str.split("; ") {
+            for pair in set.split(", ") {
+                let (num_str, color) = pair
+                    .split_once(" ")
+                    .ok_or_else(|| "Failed to split at ' '")?;
+                let num = num_str.parse::<i32>().map_err(|e| e.to_string())?;
+
+                match color {
+                    "red" => red = if num > red { num } else { red },
+                    "green" => green = if num > green { num } else { green },
+                    "blue" => blue = if num > blue { num } else { blue },
+                    _ => {}
+                }
+            }
+        }
+
+        return Ok(Self {
+            id,
+            red,
+            green,
+            blue,
+        });
+    }
+}
+
+fn parse_input(input: &str) -> i32 {
+    input
+        .split("\n")
+        .map(MaxGameRecord::try_from)
+        .flatten()
+        .filter_map(|record| {
+            let MaxGameRecord {
+                id,
+                red,
+                green,
+                blue,
+            } = record;
+
+            if red <= 12 && green <= 13 && blue <= 14 {
+                return Some(id);
+            }
+            return None;
+        })
+        .sum()
+}
+
+fn parse_input2(input: &str) -> i32 {
+    input
+        .split("\n")
+        .filter_map(|line| {
+            let MaxGameRecord {
+                id: _,
+                red,
+                green,
+                blue,
+            } = MaxGameRecord::try_from(line).ok()?;
+
+            Some(red * green * blue)
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_solve_part1() {
+        let sample_input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
+
+        assert_eq!(parse_input(sample_input), 8);
+    }
+
+    #[test]
+    fn can_solve_part2() {
+        let sample_input = r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"#;
+        assert_eq!(parse_input2(sample_input), 2286);
+    }
+}
