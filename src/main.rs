bf777103597f29c59da9a6a7c9e04b1fec1b6a37
Sun Kit Tsui <sunkittsui@gmail.com>
2023-12-02 16:51:40 -0600

    Solve 2022 day3 part2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ pub mod year_2023;
 fn main() -> Result<()> {
     let input = fs::read_to_string("data/2022/day3.data")?;
 
-    dbg!(year_2022::day3::solve_part1(&input));
+    dbg!(year_2022::day3::solve_part2(&input));
 
     Ok(())
 }
--- src/year_2022/day3.rs
+++ src/year_2022/day3.rs
@@ -1,4 +1,8 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::format,
+    vec,
+};
 
 pub fn solve_part1(input: &str) -> i32 {
     let mut map = HashMap::new();
@@ -26,6 +30,48 @@ pub fn solve_part1(input: &str) -> i32 {
         .sum()
 }
 
+pub fn solve_part2(input: &str) -> i32 {
+    let mut groups = HashMap::new();
+    let elves = input.split("\n");
+
+    let mut id = 0;
+    for (i, elf) in elves.enumerate() {
+        if i != 0 && i % 3 == 0 {
+            id += 1;
+        }
+        groups.entry(id).or_insert(Vec::new()).push(elf);
+    }
+
+    groups
+        .into_iter()
+        .filter_map(|(_, elves)| {
+            let mut masks = HashMap::new();
+            for (i, elf) in elves.iter().enumerate() {
+                elf.chars().for_each(|c| {
+                    let mask = masks.entry(c).or_insert(0);
+                    // set bit for index of elf
+                    *mask |= 1 << i;
+                });
+            }
+
+            let badge_priority = masks
+                .into_iter()
+                .filter_map(|(badge, mask)| {
+                    if mask == 0b111 {
+                        Some(item_to_priority(badge))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<i32>>()
+                .into_iter()
+                .next()?;
+
+            Some(badge_priority)
+        })
+        .sum()
+}
+
 fn item_to_priority(item: char) -> i32 {
     if item.is_uppercase() {
         item as i32 - 64 + 26
@@ -34,10 +80,6 @@ fn item_to_priority(item: char) -> i32 {
     } else {
         0
     }
-}
-
-pub fn solve_part2(_input: &str) -> i32 {
-    todo!()
 }
 
 #[cfg(test)]
@@ -62,5 +104,17 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"#;
 
         assert_eq!(solve_part1(sample_input), 157);
+    }
+
+    #[test]
+    fn can_solve_part2() {
+        let sample_input = r#"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"#;
+
+        assert_eq!(solve_part2(sample_input), 70);
     }
 }
