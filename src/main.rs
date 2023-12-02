45feeb1e78f8b8143af235c53c9f8d521f157292
Sun Kit Tsui <sunkittsui@gmail.com>
2023-12-01 23:56:26 -0600

    Solve 2022 day2 part2

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ pub mod year_2022;
 fn main() -> Result<()> {
     let input = fs::read_to_string("data/2022/day2.data")?;
 
-    dbg!(year_2022::day2::solve_part1(&input));
+    dbg!(year_2022::day2::solve_part2(&input));
 
     Ok(())
 }
