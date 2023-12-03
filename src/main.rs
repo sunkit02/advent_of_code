f08ba6eb43df8d42930b791e55bed257f5bcd8bb
Sun Kit Tsui <sunkittsui@gmail.com>
2023-12-03 15:48:25 -0600

    Solve 2023 day3 part2

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,7 @@ pub mod year_2023;
 fn main() -> Result<()> {
     let input = fs::read_to_string("data/2023/day3.data")?;
 
-    let highest_sum = input
-        .split(|c: char| !c.is_numeric())
-        .filter(|s| s.len() > 0)
-        .map(|s| s.parse::<i32>())
-        .flatten()
-        .sum::<i32>();
-
-    dbg!(highest_sum);
-
-    println!("Output: {}", year_2023::day3::solve_part1(&input));
+    println!("Output: {}", year_2023::day3::solve_part2(&input));
 
     Ok(())
 }
--- src/year_2023/day3.rs
+++ src/year_2023/day3.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn solve_part1(input: &str) -> i32 {
     let schema: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
@@ -94,6 +96,145 @@ pub fn solve_part1(input: &str) -> i32 {
     return sum;
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Point(usize, usize);
+
+pub fn solve_part2(input: &str) -> i32 {
+    let schema: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    // gear pt -> set of the pts of nums adjacent to it
+    let mut gears: HashMap<Point, HashSet<Point>> = HashMap::new();
+    let mut num_strs: HashMap<Point, String> = HashMap::new();
+
+    let mut num_pt: Option<Point> = None;
+
+    for (y, line) in schema.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if c.is_numeric() {
+                num_strs
+                    .entry(num_pt.get_or_insert(Point(x, y)).clone())
+                    .or_insert(String::new())
+                    .push(*c);
+
+                if x > 0 {
+                    // check left
+                    if is_gear(schema[y][x - 1]) {
+                        if let Some(pt) = num_pt {
+                            gears
+                                .entry(Point(x - 1, y))
+                                .or_insert(HashSet::new())
+                                .insert(pt);
+                        }
+                    }
+
+                    if y > 0 {
+                        // check up left
+                        if is_gear(schema[y - 1][x - 1]) {
+                            if let Some(pt) = num_pt {
+                                gears
+                                    .entry(Point(x - 1, y - 1))
+                                    .or_insert(HashSet::new())
+                                    .insert(pt);
+                            }
+                        }
+                    }
+
+                    if y < schema.len() - 1 {
+                        // check down left
+                        if is_gear(schema[y + 1][x - 1]) {
+                            if let Some(pt) = num_pt {
+                                gears
+                                    .entry(Point(x - 1, y + 1))
+                                    .or_insert(HashSet::new())
+                                    .insert(pt);
+                            }
+                        }
+                    }
+                }
+
+                if y > 0 {
+                    // check up
+                    if is_gear(schema[y - 1][x]) {
+                        if let Some(pt) = num_pt {
+                            gears
+                                .entry(Point(x, y - 1))
+                                .or_insert(HashSet::new())
+                                .insert(pt);
+                        }
+                    }
+                }
+
+                if y < schema.len() - 1 {
+                    // check down
+                    if is_gear(schema[y + 1][x]) {
+                        if let Some(pt) = num_pt {
+                            gears
+                                .entry(Point(x, y + 1))
+                                .or_insert(HashSet::new())
+                                .insert(pt);
+                        }
+                    }
+                }
+
+                if x < line.len() - 1 {
+                    // check right
+                    if is_gear(schema[y][x + 1]) {
+                        if let Some(pt) = num_pt {
+                            gears
+                                .entry(Point(x + 1, y))
+                                .or_insert(HashSet::new())
+                                .insert(pt);
+                        }
+                    }
+
+                    if y > 0 {
+                        // check up right
+                        if is_gear(schema[y - 1][x + 1]) {
+                            if let Some(pt) = num_pt {
+                                gears
+                                    .entry(Point(x + 1, y - 1))
+                                    .or_insert(HashSet::new())
+                                    .insert(pt);
+                            }
+                        }
+                    }
+
+                    if y < schema.len() - 1 {
+                        // check down right
+                        if is_gear(schema[y + 1][x + 1]) {
+                            if let Some(pt) = num_pt {
+                                gears
+                                    .entry(Point(x + 1, y + 1))
+                                    .or_insert(HashSet::new())
+                                    .insert(pt);
+                            }
+                        }
+                    }
+                }
+            } else {
+                num_pt.take();
+            }
+        }
+    }
+
+    let mut sum = 0;
+
+    for (_, part_nums) in gears {
+        if part_nums.len() == 2 {
+            sum += part_nums
+                .into_iter()
+                .filter_map(|pt| num_strs.get(&pt)?.parse::<i32>().ok())
+                .product::<i32>();
+        }
+    }
+
+    return sum;
+}
+
+fn is_gear(c: char) -> bool {
+    return c == '*';
+}
+
 fn is_symbol(c: char) -> bool {
     match c {
         '`' => true,
@@ -130,9 +271,6 @@ fn is_symbol(c: char) -> bool {
         _ => false,
     }
 }
-pub fn solve_part2(_input: &str) -> i32 {
-    todo!()
-}
 
 #[cfg(test)]
 mod tests {
@@ -155,7 +293,7 @@ mod tests {
     }
 
     #[test]
-    fn can_solve_part1_changed() {
+    fn can_solve_part2() {
         let sample_input = r#"467..114..
 ...*......
 ..35..633.
@@ -167,6 +305,6 @@ mod tests {
 ...$.*....
 .664.598.."#;
 
-        assert_eq!(solve_part1(sample_input), 4361);
+        assert_eq!(solve_part2(sample_input), 467835);
     }
 }
