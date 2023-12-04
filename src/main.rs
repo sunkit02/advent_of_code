51060cd689495648d833bde75b23a5879e922ebe
Sun Kit Tsui <sunkittsui@gmail.com>
2023-12-04 15:31:27 -0600

    Solve 2022 day8 part1

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ pub mod year_2022;
 pub mod year_2023;
 
 fn main() -> Result<()> {
-    let input = fs::read_to_string("data/2023/day4.data")?;
+    let input = fs::read_to_string("data/2022/day8.data")?;
 
-    println!("Output: {:?}", year_2023::day4::solve_part2(&input));
+    println!("Output: {:?}", year_2022::day8::solve_part1(&input));
 
     Ok(())
 }
---
+++ src/year_2022/day8.rs
@@ -0,0 +1,74 @@
+const ASCII_NUM_OFFSET: i32 = 48;
+
+pub fn solve_part1(input: &str) -> i32 {
+    let trees: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| line.chars().map(|c| c as i32 - ASCII_NUM_OFFSET).collect())
+        .collect();
+
+    let mut visible_count = 0;
+
+    for (y, row) in trees.iter().enumerate() {
+        for (x, tree) in row.iter().enumerate() {
+            // Tree on edge
+            if x == 0 || y == 0 || x == row.len() - 1 || y == trees.len() - 1 {
+                visible_count += 1;
+                continue;
+            }
+
+            // Check left
+            if let Some(height) = trees[y][..x].iter().max() {
+                if height < tree {
+                    visible_count += 1;
+                    continue;
+                }
+            }
+
+            // Check right
+            if let Some(height) = trees[y][x + 1..].iter().max() {
+                if height < tree {
+                    visible_count += 1;
+                    continue;
+                }
+            }
+
+            // Check up
+            if let Some(height) = trees[..y].iter().map(|row| row[x]).max() {
+                if height < *tree {
+                    visible_count += 1;
+                    continue;
+                }
+            }
+
+            // Check down
+            if let Some(height) = trees[y + 1..].iter().map(|row| row[x]).max() {
+                if height < *tree {
+                    visible_count += 1;
+                    continue;
+                }
+            }
+        }
+    }
+
+    visible_count
+}
+
+pub fn solve_part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_solve_part1() {
+        let sample_input = r#"30373
+25512
+65332
+33549
+35390"#;
+
+        assert_eq!(solve_part1(sample_input), 21);
+    }
+}
